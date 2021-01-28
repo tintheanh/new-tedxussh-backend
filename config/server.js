dd0d80ef45da576feed505b6e9e686ca84de4542
Anh Nguyen <tintheanh@gmail.com>
2021-01-27 21:33:02 -0800

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'e49b5880b42acc4a53407f9c50c04691'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '9e43f5b3-aa74-43c9-9e9b-bfef05529d1a'
+};
